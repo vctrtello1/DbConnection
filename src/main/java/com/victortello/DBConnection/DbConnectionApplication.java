0e9f50215b20013cbf31270e1676b9fb8d33abd7
Victor Hugo Tello <victorhugotello@hotmail.com>
2022-05-25 13:26:33 -0500

    first commit

---
+++ src/main/java/com/victortello/DBConnection/DbConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.victortello.DBConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectionApplication.class, args);
+	}
+
+}
